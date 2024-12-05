b88fe3dae8cf232e878191a0180bb1f832900a1e
Vipintro <119474150+Vipintro@users.noreply.github.com>
2024-12-05 19:56:30 +0700

    Add files via upload

--- FE/register.php
+++ FE/register.php
@@ -12,7 +12,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="FE/css/style.css">
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
@@ -23,10 +23,12 @@
    
 <div class="form-container">
 
-   <form action="include/register.inc.php" method="post">
+   <form action="" method="post">
       <h3>register now</h3>
-      <input type="text" name="name" placeholder="enter your name" required class="box">
+      <input type="text" name="username" placeholder="enter your username" required class="box">
       <input type="email" name="email" placeholder="enter your email" required class="box">
+      <input type="text" name="fname" placeholder="enter your fname" required class="box">
+      <input type="text" name="lname" placeholder="enter your lname" required class="box">
       <input type="password" name="password" placeholder="enter your password" required class="box">
       <input type="password" name="cpassword" placeholder="confirm your password" required class="box">
       <select name="user_type" class="box">
@@ -35,7 +37,7 @@
          <option value="Admin">Admin</option>
       </select>
       <input type="submit" name="submit" value="register now" class="btn">
-      <p>Already have an account? <a href="index.php?page=login">Login now</a></p>
+      <p>already have an account? <a href="login.php">login now</a></p>
    </form>
 
 </div>
